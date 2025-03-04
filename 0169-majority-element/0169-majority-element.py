3436cc79afa99a3e0ec5f18a834f17782a2dee9a
Md. Muntasir Azad <138347643+MuntasirAraf14@users.noreply.github.com>
2025-03-04 12:05:32 +0600

    Time: 10 ms (32.42%), Space: 19.5 MB (20.02%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,11 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hash= {}
-        res = majority = 0
+        hash = {}
+        a = major = 0
 
         for n in nums:
-            hash[n] = 1 + hash.get(n,0)
-            if hash[n] > majority:
-                res = n
-                majority = hash[n]
-        return res
+            hash[n] = 1 + hash.get(n, 0)
+
+            if hash[n] > major:
+                a = n
+                major = hash[n]
+
+        return a
